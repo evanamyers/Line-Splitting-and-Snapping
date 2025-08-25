3d2800be74e9d16e90675ad9d074caf01ed21885
evanamyers <58479443+evanamyers@users.noreply.github.com>
2025-08-25 09:48:52 -0400

    Update Reshape Line at Intersecting Point.py

--- Reshape Line at Intersecting Point.py
+++ Reshape Line at Intersecting Point.py
@@ -1,9 +1,6 @@
 import arcpy
-import os, sys
-import numpy as np
 from shapely.geometry import Point, LineString
 from shapely import STRtree, snap
-import datetime
 
 sys.tracebacklimit = 1000
 
@@ -13,23 +10,30 @@ pointLayer = arcpy.GetParameterAsText(1)
 tolerance = arcpy.GetParameter(2)
 
 
-# startTime = datetime.datetime.now().replace(microsecond=0)
-# arcpy.AddMessage(f'Operation started on {startTime}')
-# print(f'Operation started on {startTime}')
+def findWorkspace(fc):
+    desc = arcpy.Describe(fc)
+    fullPath = desc.catalogPath
+
+    if '.sde' in fullPath:
+        return fullPath.rsplit('.sde', 1)[0] + '.sde'
+    elif '.gdb' in fullPath:
+        return fullPath.rsplit('.gdb', 1)[0] + '.gdb'
+    elif fullPath.lower().startswith("http"):
+        # Hosted feature service or REST URL
+        return fullPath  # or return None if you just want to skip these
+    else:
+        # Default fallback
+        return arcpy.env.workspace
 
 
 pointList = []
-pointArrayList = []
 with arcpy.da.SearchCursor(pointLayer, ['OID@', 'SHAPE@XY']) as cursor:
     for row in cursor:
         pointList.append(Point(row[1]))
-        pointArrayList.append(row[1])
 
 pointTree = STRtree(pointList)
-pointArray = np.array(pointArrayList)
 
 lineDict = {}
-lineArrayDict = {}
 with arcpy.da.SearchCursor(lineLayer, ["OID@", "SHAPE@"]) as cursor:
     for line in cursor:
         lineCoords = []
@@ -37,7 +41,6 @@ with arcpy.da.SearchCursor(lineLayer, ["OID@", "SHAPE@"]) as cursor:
             for coord in vert:
                 lineCoords.append((coord.X, coord.Y))
             lineDict[line[0]] = LineString(lineCoords)
-            lineArrayDict[line[0]] = np.array(lineCoords)
 
 snapPointDict = {}
 for key, line in lineDict.items():
@@ -52,15 +55,14 @@ for key, line in lineDict.items():
         new_geom = arcpy.FromWKB(newLine.wkb)
         snapPointDict[key] = new_geom
 
-
-lineDesc = arcpy.Describe(lineLayer)
-workspace = os.path.dirname(arcpy.Describe(lineLayer).catalogPath)
-desc = arcpy.Describe(workspace)
-if hasattr(desc, "datasetType") and desc.datasetType == 'FeatureDataset':
-    workspace = os.path.dirname(workspace)
 count = 0
 try:
-    with arcpy.da.Editor(workspace, multiuser_mode=lineDesc.isVersioned):
+
+    workspace_fc = findWorkspace(lineLayer)
+    TableDesc = arcpy.Describe(lineLayer)
+
+    # with arcpy.da.Editor(workspace, multiuser_mode=lineDesc.isVersioned):
+    with arcpy.da.Editor(workspace_fc, multiuser_mode=TableDesc.isVersioned):
         with arcpy.da.UpdateCursor(lineLayer, ["OID@", "SHAPE@"]) as cursor:
             for row in cursor:
                 if row[0] in snapPointDict:
@@ -72,15 +74,11 @@ try:
                         cursor.updateRow((row[0], snapPointDict[row[0]]))
 
     arcpy.management.ApplySymbologyFromLayer(lineLayer, lineLayer, update_symbology="MAINTAIN")
+
+    del pointList, pointTree, lineDict, snapPointDict
+
 except SystemError:
     print('Edits need to be saved before running tool.')
     raise SystemError('Edits need to be saved before running tool.') from None
 
-
-# endTime = datetime.datetime.now().replace(microsecond=0)
-# dur = endTime - startTime
-# dur = str(dur)
-# arcpy.AddMessage(f'Fixed {count} lines.')
-# arcpy.AddMessage(f'Duration: {dur}')
-# print(f'Duration: {dur}')
 
